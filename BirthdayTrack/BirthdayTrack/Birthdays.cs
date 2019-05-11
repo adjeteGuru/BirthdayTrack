175537acd8216ffe03c68d9163be31a3d7ac901c
adjeteGuru <justinadjev@hotmail.com>
2019-05-11 14:18:12 +0100

    set person and birthday classes

---
+++ BirthdayTrack/BirthdayTrack/Birthdays.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayTrack
+{// birthdays class to hold information about people
+ public class Birthdays
+    {
+        //declare and reference a member person
+        int currenPerson = 0;
+
+        //this is object people to hold list of people
+        List<Person> people = new List<Person>();
+
+        //access a person data
+        public string GetPersonFirstName()
+        {
+          return people[currenPerson].FirstName;
+        }
+
+        //access a person data
+        public string GetPersonLastName()
+        {
+            return people[currenPerson].LastName;
+        }
+
+        //access a person data
+        public string GetPersonDateOfBirth()
+        {
+            return people[currenPerson].DateOfBirth();
+        }
+    }
+}
---
+++ BirthdayTrack/BirthdayTrack/Person.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayTrack
+{
+    //class to hold information about a person
+   public class Person
+    {
+        //attributes of the person member of the class person
+        private string mFirstName;
+        private string mLastName;
+        private int mDayBorn;
+        private int mMonthBorn;
+        private int mYearBorn;
+    
+  //construction DayOfBirth
+  public string DateOfBirth()
+        {
+            return string.Format("{0}/{1}/{2}", mDayBorn, mMonthBorn, mYearBorn);
+        }
+
+    //encapsulation Get Set FirstName to be call upon
+    public string FirstName
+    {
+        get
+        {
+            return mFirstName;
+        }
+        set
+        {
+            value = mFirstName;
+        }
+    }
+
+        //encapsulation Get Set LastName to be call upon
+        public string LastName
+        {
+            get
+            {
+                return mLastName;
+            }
+            set
+            {
+                value = mLastName;
+            }
+
+        }
+
+        //encapsulation Get Set DayBorn to be call upon
+        public int DayBorn
+        {
+            get
+            {
+                return mDayBorn;
+            }
+            set
+            {
+                value = mDayBorn;
+            }
+
+        }
+
+        //encapsulation Get Set MonthBorn to be call upon
+        public int MonthBorn
+        {
+            get
+            {
+                return mMonthBorn;
+            }
+            set
+            {
+                value = mMonthBorn;
+            }
+
+        }
+
+        //encapsulation Get Set YearBorn to be call upon
+        public int YearBorn
+        {
+            get
+            {
+                return mYearBorn;
+            }
+            set
+            {
+                value = mYearBorn;
+            }
+        }
+
+  
+    }
+}
